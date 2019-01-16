9849d07c2e7c954e3937c1c2c4c6bca9e17b09de
DmitryZagorulko <zagorulko@retailcrm.ru>
2019-01-16 16:49:41 +0300

    improve channels request struct

--- v1/types.go
+++ v1/types.go
@@ -176,14 +176,14 @@ type ChannelListItem struct {
 
 // Channels request type
 type Channels struct {
-	ID          int       `url:"id"`
-	Types       []string  `url:"types"`
-	Active      bool      `url:"active"`
-	Since       time.Time `url:"since"`
-	Until       time.Time `url:"until"`
-	TransportID uint64    `url:"transport_id"`
-	Sort        string    `url:"sort"`
-	Limit       int       `url:"limit"`
+	ID          int       `url:"id" json:"id,omitempty"`
+	Types       []string  `url:"types" json:"types,omitempty"`
+	Active      bool      `url:"active" json:"active,omitempty"`
+	Since       time.Time `url:"since" json:"since,omitempty"`
+	Until       time.Time `url:"until" json:"until,omitempty"`
+	TransportID uint64    `url:"transport_id" json:"transport_id,omitempty"`
+	Sort        string    `url:"sort" json:"sort,omitempty"`
+	Limit       int       `url:"limit" json:"limit,omitempty"`
 }
 
 // User struct
