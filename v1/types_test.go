ebded3077b4cdf3e8761b2bc2af2dc5bd22f31a3
DanielWeiser <46303277+DanielWeiser@users.noreply.github.com>
2021-01-15 14:51:24 +0300

    TransportResponse types (#70)

--- v1/types_test.go
+++ v1/types_test.go
@@ -99,3 +99,43 @@ func TestOriginator(t *testing.T) {
 
 	t.Run("MarshalJSON_Invalid", OriginatorMarshalJSONInvalid)
 }
+
+func TestTransportErrorResponse(t *testing.T) {
+	t.Run("NewTransportErrorResponse", func(t *testing.T) {
+		expected := TransportResponse{
+			Error: &TransportError{
+				Code:    MessageErrorGeneral,
+				Message: "error",
+			},
+		}
+		resp := NewTransportErrorResponse(MessageErrorGeneral, "error")
+		assert.Equal(t, expected, resp)
+	})
+
+	t.Run("NewSentMessageResponse", func(t *testing.T) {
+		expected := TransportResponse{
+			ExternalMessageID: "extID",
+		}
+		resp := NewSentMessageResponse("extID")
+		assert.Equal(t, expected, resp)
+	})
+
+	t.Run("MarshalJSON", func(t *testing.T) {
+		cases := []struct {
+			byte     byte
+			expected string
+		}{
+			{1, "general"},
+			{2, "customer_not_exists"},
+			{3, "reply_timed_out"},
+			{4, "spam_suspicion"},
+			{5, "access_restricted"},
+		}
+		for _, c := range cases {
+			result, err := TransportErrorCode(c.expected).MarshalJSON()
+			expected := []byte(fmt.Sprintf(`"%s"`, c.expected))
+			assert.NoError(t, err)
+			assert.Equal(t, expected, result)
+		}
+	})
+}
