d2d285b3b24e421c812c0d2bdc3bffea21f5c07f
Ефанов Руслан <efanov@retailcrm.ru>
2023-05-05 14:58:26 +0300

    fixed code style

--- v1/template.go
+++ v1/template.go
@@ -37,15 +37,15 @@ var templateVarAssoc = map[string]interface{}{
 
 // Template struct.
 type Template struct {
-	Code      string         `json:"code"`
-	ChannelID uint64         `json:"channel_id,omitempty"`
-	Name      string         `json:"name"`
-	Enabled   bool           `json:"enabled,omitempty"`
-	Type      string         `json:"type"`
-	Template  []TemplateItem `json:"template"`
-	HeaderParams *HeaderParams `json:"headerParams,omitempty"`
-	Footer *string `json:"footer,omitempty"`
-	ButtonParams []ButtonParam `json:"buttonParams,omitempty"`
+	Code         string         `json:"code"`
+	ChannelID    uint64         `json:"channel_id,omitempty"`
+	Name         string         `json:"name"`
+	Enabled      bool           `json:"enabled,omitempty"`
+	Type         string         `json:"type"`
+	Template     []TemplateItem `json:"template"`
+	HeaderParams *HeaderParams  `json:"headerParams,omitempty"`
+	Footer       *string        `json:"footer,omitempty"`
+	ButtonParams []ButtonParam  `json:"buttonParams,omitempty"`
 }
 
 // TemplateItem is a part of template.
