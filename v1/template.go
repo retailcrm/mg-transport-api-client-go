35957ff156ce570b452acca50abc4a838d47b63f
Суханов Данила <suhanov@retailcrm.ru>
2023-10-16 15:35:02 +0300

    add rejected_reason and verification_status fields for template

--- v1/template.go
+++ v1/template.go
@@ -37,17 +37,19 @@ var templateVarAssoc = map[string]interface{}{
 
 // Template struct.
 type Template struct {
-	Code         string         `json:"code"`
-	ChannelID    uint64         `json:"channel_id,omitempty"`
-	Name         string         `json:"name"`
-	Enabled      bool           `json:"enabled,omitempty"`
-	Type         string         `json:"type"`
-	Template     []TemplateItem `json:"template"`
-	HeaderParams *HeaderParams  `json:"headerParams,omitempty"`
-	Footer       *string        `json:"footer,omitempty"`
-	ButtonParams []ButtonParam  `json:"buttonParams,omitempty"`
-	Lang         string         `json:"lang,omitempty"`
-	Category     string         `json:"category,omitempty"`
+	Code               string         `json:"code"`
+	ChannelID          uint64         `json:"channel_id,omitempty"`
+	Name               string         `json:"name"`
+	Enabled            bool           `json:"enabled,omitempty"`
+	Type               string         `json:"type"`
+	Template           []TemplateItem `json:"template"`
+	HeaderParams       *HeaderParams  `json:"headerParams,omitempty"`
+	Footer             *string        `json:"footer,omitempty"`
+	ButtonParams       []ButtonParam  `json:"buttonParams,omitempty"`
+	Lang               string         `json:"lang,omitempty"`
+	Category           string         `json:"category,omitempty"`
+	RejectedReason     string         `json:"rejected_reason,omitempty"`
+	VerificationStatus string         `json:"verification_status,omitempty"`
 }
 
 // TemplateItem is a part of template.
